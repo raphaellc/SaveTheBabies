647d55a7c7820887fbd0b661bd65044833f151ed
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2018-06-20 01:28:49 -0300

    StartingProject
    Projeto Compilando
    Projeto Incluindo um recurso.

---
+++ SaveTheBabies/Source/Jogo.cpp
@@ -0,0 +1,39 @@
+#include "Jogo.h"
+
+Jogo::Jogo()
+{
+}
+
+Jogo::~Jogo()
+{
+}
+
+void Jogo::inicializar()
+{
+	uniInicializar(800, 600, false);
+
+	//	O resto da inicialização vem aqui!
+	//	...
+	gRecursos.carregarSpriteSheet("baby", "../../_assets/Graphics/Baby/baby0004.png", 1, 1, QUALIDADE_ESCALA_BAIXA);
+}
+
+void Jogo::finalizar()
+{
+	//	O resto da finalização vem aqui (provavelmente, em ordem inversa a inicialização)!
+	//	...
+
+	uniFinalizar();
+}
+
+void Jogo::executar()
+{
+	while(!gTeclado.soltou[TECLA_ESC] && !gEventos.sair)
+	{
+		uniIniciarFrame();
+
+		//	Seu código vem aqui!
+		//	...
+
+		uniTerminarFrame();
+	}
+}
